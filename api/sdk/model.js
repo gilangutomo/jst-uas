687c9d9c3670d1994252b29801b1add492eb3f01
gilang <85244821+gilangutomo@users.noreply.github.com>
2021-07-23 21:09:54 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,24 +1,24 @@
 const tf = require('@tensorflow/tfjs-node');
 
 function normalized(data){ // i & r
-    x1 = (data[0] - -1.720494425) / 42.773
-    x2 = (data[1] - 1.349425519) / 29.9412
-    x3 = (data[2] - -0.888496155) / 94.8964
-    x4 = (data[3] - -0.877496155) / 94.4343
-    return [x1, x2, x3, x4]
+    x1 = (data[0] - 42.773) / 10.33017
+    x2 = (data[1] - 29.9412) / 8.936247
+    x3 = (data[2] - 94.8964) / 8.887377
+
+    return [x1, x2, x3]
 }
 
 function denormalized(data){
-    x1 = (data[0] * 42.773) + 10.33017006
-    x2 = (data[1] * 29.9412) + 8.936247191
-    x3 = (data[2] * 94.8964) + 8.887376673
-    x4 = (data[3] * 84.8964) + 8.899376673
-    return [x1, x2, x3, x4]
+    y1 = (data[0] * 16.08198) + 32.2718
+    y2 = (data[1] * 8.918185) + 39.959
+    y3 = (data[2] * 11.79956) + 69.739
+
+    return [y1, y2, y3]
 }
 
 
 async function predict(data){
-    let in_dim = 4;
+    let in_dim = 3;
     
     data = normalized(data);
     shape = [1, in_dim];
@@ -27,7 +27,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/gilangutomo/jst-uas/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Afaizin-bitt/UAS_JST/main/public/Model5/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
@@ -44,4 +44,3 @@ async function predict(data){
 module.exports = {
     predict: predict 
 }
-  
